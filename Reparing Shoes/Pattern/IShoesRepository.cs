bd668dae2902df37a12870d2c668b0b27e2dddb9
tursunboev <hayotillotursunboev7@gmail.com>
2024-02-15 18:49:51 +0500

    Add project files.

---
+++ Reparing Shoes/Pattern/IShoesRepository.cs
@@ -0,0 +1,13 @@
+﻿using Reparing_Shoes.DTOModels;
+using Reparing_Shoes.Models;
+
+namespace Reparing_Shoes.Pattern
+{
+    public interface IShoesRepository
+    {
+        public ShoesDTO Post(ShoesDTO shoes);
+        public IEnumerable<Shoes> GetAllShoes();
+        
+
+    }
+}
